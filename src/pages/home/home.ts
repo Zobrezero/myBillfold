f135892f267579cdc17b1588533ca88cc9fce933
Jesús Coronado <zobrezero2011@gmail.com>
2018-07-04 21:36:21 -0300

    Primer boceteado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,107 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {
+  NavController,
+  AlertController,
+} from 'ionic-angular';
 
-@Component({
+import { IntroPage } from '../intro/intro';
+
+export interface Cost
+{
+  quantity:number;
+  plus: boolean;
+}
+
+@Component(
+{
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage
+{
+  public firstime:boolean = true;
+  public registered:Cost[] = [];
+  public total:number = 0;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+  ){
+  	if (this.firstime)
+    {
+      this.firstime = false;
+      this.navCtrl.push(IntroPage);
+  	}
   }
 
+  addCost(q:Cost)
+  {
+    this.total += q.plus ? q.quantity * 1 : q.quantity * (-1);
+    this.registered = this.registered.concat([q]);
+    this.registered.reverse();
+  }
+
+  showAddModifier(event)
+  {
+    var par = this;
+    const prompt = this.alertCtrl.create(
+    {
+      title: 'Ingreso',
+      message: 'Está por agregar un monto que suma',
+      inputs:
+      [
+        {
+          name: 'val',
+          type: 'number',
+          placeholder: '$'
+        },
+      ],
+      buttons:
+      [
+        {
+          text: 'cancelar',
+        },
+        {
+          text: 'aplicar',
+          handler: data =>
+          {
+            par.addCost({quantity: data.val, plus: true});
+          }
+        },
+      ]
+    });
+    prompt.present();
+  }
+
+  showRemoveModifier(event)
+  {
+    var par = this;
+    const prompt = this.alertCtrl.create(
+    {
+      title: 'Egreso',
+      message: 'Está por registrar un gasto',
+      inputs:
+      [
+        {
+          name: 'val',
+          type: 'number',
+          placeholder: '$'
+        },
+      ],
+      buttons:
+      [
+        {
+          text: 'cancelar',
+        },
+        {
+          text: 'Aplicar',
+          handler: data =>
+          {
+            par.addCost({quantity: data.val, plus: false});
+          }
+        },
+      ]
+    });
+    prompt.present();
+  }
 }
